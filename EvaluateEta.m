e1b40e8de8d5b13c3fdd306c65f3f8917f15e1df
serdan@serdan-GE62-6QL <shaerdan@gmail.com>
2020-06-01 14:09:52 +0100

    Latest Updates

--- EvaluateEta.m
+++ EvaluateEta.m
@@ -17,8 +17,8 @@ for i=1:Mesh.LengthRadiusS
                 sin(Mesh.ThetaQ).*cos(Mesh.PsiS(k)- Mesh.PsiQ);
             DistanceSQ  = sqrt(1 + Mesh.RadiusS(i).^2 - 2* Mesh.RadiusS(i).*CosGamma);
             F      = (2./DistanceSQ) - log(1-Mesh.RadiusS(i).*CosGamma + DistanceSQ);
-            Integrand0 = F.*sin(Mesh.ThetaQ);
-            FL2Norm = trapz(Mesh.PsiLine', trapz(Mesh.ThetaLine',Integrand0,2));
+            Integrand0 = (F.^2)*sin(Mesh.ThetaQ);
+            FL2Norm = sqrt(trapz(Mesh.PsiLine', trapz(Mesh.ThetaLine',Integrand0,2)));
             F_Normalized  = F./FL2Norm;
             Integrand1 = F_Normalized.*P.*sin(Mesh.ThetaQ);
             Eta(i,j,k) = trapz(Mesh.PsiLine', trapz(Mesh.ThetaLine',Integrand1,2));
