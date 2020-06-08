7918dcf8872d6844c71045a17301befb34038b9b
serdan@serdan-GE62-6QL <shaerdan@gmail.com>
2020-06-08 07:45:51 +0100

    Updated

--- GenerateMesh.m
+++ GenerateMesh.m
@@ -1,4 +1,4 @@
-function [Mesh] = GenerateMesh(MeshPointSSize,MeshPointQSize,RadiusCap)
+function [Mesh] = GenerateMesh(MeshPointSSize,MeshPointQSize,Cap)
 %UNTITLED Summary of this function goes here
 %   Generate Mesh
 %   Mesh.ThetaQ:  a 2d matrix meshing the theta commponent of the point Q \in R^2;
@@ -8,15 +8,15 @@ function [Mesh] = GenerateMesh(MeshPointSSize,MeshPointQSize,RadiusCap)
 %   Mesh.PsiS:    a 3d array(tensor) meshing of psi commponent of the point S \in R^3;
 
 SIncreSizeRadius = 1/MeshPointSSize.Radius;
-SIncreSizeTheta = pi/MeshPointSSize.Theta;
-SIncreSizePsi = 2*pi/MeshPointSSize.Psi;
+SIncreSizeTheta = Cap.Theta/MeshPointSSize.Theta;
+SIncreSizePsi = Cap.Psi/MeshPointSSize.Psi;
 QIncreSizeTheta = pi/MeshPointQSize.Theta;
 QIncreSizePsi = 2*pi/MeshPointQSize.Psi;
 Mesh.ThetaQLine =  0:QIncreSizeTheta:pi;
 Mesh.PsiQLine = 0:QIncreSizePsi:2*pi;
-Mesh.RadiusS = 0:SIncreSizeRadius:RadiusCap; 
-Mesh.ThetaS = 0:SIncreSizeTheta:pi;
-Mesh.PsiS  =  0:SIncreSizePsi:2*pi;
+Mesh.RadiusS = 0:SIncreSizeRadius:Cap.Radius; 
+Mesh.ThetaS = 0:SIncreSizeTheta:Cap.Theta;
+Mesh.PsiS  =  0:SIncreSizePsi:Cap.Psi;
 [Mesh.ThetaQ,Mesh.PsiQ] = meshgrid(Mesh.ThetaQLine,Mesh.PsiQLine);
 Mesh.LengthRadiusS = length(Mesh.RadiusS); 
 Mesh.LengthThetaS = length(Mesh.ThetaS); 
--- ObjectiveFuncNonLinearFminunc.m
+++ ObjectiveFuncNonLinearFminunc.m
@@ -1,19 +1,24 @@
 function [J,GradientPseudo] = ObjectiveFuncNonLinearFminunc(X,Measurement,Lambda, Mesh)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
+SIN1 = sin(Mesh.ThetaQ);
+dx = Mesh.ThetaQLine';
+dy = Mesh.PsiQLine';
+
 SourceNum = length(X)/4;
 Locations = X(SourceNum +1:end);
 FL2Norm = L2NormF(Locations,Mesh);
-IntensityNormalized = X(1:SourceNum)./FL2Norm;
+IntensityNormalized = X(1:SourceNum)./FL2Norm';
 
 [PhiComponent,CosGamma,DistPQ] = ComputePotentialComponent(Locations,Mesh);
 
 Estimation=sum(bsxfun(@times,PhiComponent,reshape(IntensityNormalized,1,1,SourceNum)),3);
 Descrepency = Estimation - Measurement;
-LeastSquareTerm = norm(Descrepency,2);
+IntegrandLeastSquare = (Descrepency.^2)*SIN1;
+LeastSquareTerm = trapz(dy,trapz(dx,IntegrandLeastSquare,2));
 RegularizationTerm = norm(IntensityNormalized,1);
 
-J = 0.5*LeastSquareTerm^2 + Lambda*RegularizationTerm;
+J = 0.5*LeastSquareTerm + Lambda*RegularizationTerm;
 [GradientPseudo] = GradPseudoFminunc(X,Descrepency,PhiComponent,CosGamma,DistPQ,Mesh,FL2Norm, Lambda);
 
 end
